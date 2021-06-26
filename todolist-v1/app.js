25590f8db2fcd47e7e17b67e5158d6a088cd55c8
Andrew <alarkins93@yahoo.com>
2021-06-25 22:22:32 -0400

    finish todolist

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,17 +5,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  var currentDay = daysOfWeek[ today.getDay() ];
+  // var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  // var currentDay = daysOfWeek[ today.getDay() ];
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  var day = today.toLocaleDateString("en-US", options);
+
   res.render("list", {
-    day: currentDay
+    kindOfDay: day,
+    newListItems: items
   });
 
 });
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
