40dd9222bec1602a20de5b1c48f73f8e4ad61ebe
Andrew <alarkins93@yahoo.com>
2021-06-27 10:04:07 -0400

    finish todolist

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,44 +2,60 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
-var items = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  // var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  // var currentDay = daysOfWeek[ today.getDay() ];
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-  var day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 
-
-
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 
 app.listen(3000, function() {
