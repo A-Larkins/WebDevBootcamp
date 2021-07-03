845699cae18676a4e9f2379875f44f84ff418400
Andrew <alarkins93@yahoo.com>
2021-07-03 09:52:48 -0400

    finish fruits project with native mongoDB driver

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -7,7 +7,7 @@ const url = 'mongodb://localhost:27017';
 
 const dbName = 'fruitsDB';
 
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(function(err) {
   assert.equal(null, err);
@@ -15,5 +15,42 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  // insertDocuments(db, function() {
+  //   client.close();
+  // });
+  findDocuments(db, function() {
+    client.close();
+  });
+
 });
+
+const insertDocuments = function(db, callback) {
+  const collection = db.collection('fruits');
+  collection.insertMany([
+    {name: "Apple",
+    score: 9,
+    review: "Keeps doctor away"},
+    {name: "Kiwi",
+    score: 9,
+    review: "Pretty Great"},
+    {name: "Strawberry",
+    score: 10,
+    review: "One of my favs"}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
+
+const findDocuments = function(db, callback) {
+  const collection = db.collection('fruits');
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits);
+    callback(fruits);
+  })
+};
